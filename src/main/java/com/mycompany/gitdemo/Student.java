a8c8f1a1110f2c881ec30e3502db3ee3dcf3d064
Admin <Admin@DESKTOP-QSE98BN>
2020-09-08 21:54:01 +0700

    version1

--- src/main/java/com/mycompany/gitdemo/Student.java
+++ src/main/java/com/mycompany/gitdemo/Student.java
@@ -10,21 +10,7 @@ public class Student {
         
     }
 
-    public int getStudentId() {
-        return studentId;
-    }
     
-    public void setStudentId(int studentId) {
-        this.studentId = studentId;
-    }
-
-    public String getStudentName() {
-        return studentName;
-    }
-
-    public void setStudentName(String studentName) {
-        this.studentName = studentName;
-    }
-    
+   
     
 }
