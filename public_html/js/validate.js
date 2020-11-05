5391848c6bef28c147c211843fbf9dbd13511c5c
Admin <Admin@DESKTOP-QSE98BN>
2020-11-05 19:45:50 +0700

    oto

---
+++ public_html/js/validate.js
@@ -0,0 +1,37 @@
+$(function () {
+    $('#form-login').validate({
+        rules: {
+            email: {
+                required: true,
+                email: true
+            },
+            password: {
+                required: true,
+                minlength: 8
+            },
+            request: {
+                required: true,
+                minlength: 8
+            }
+        },
+        messages: {
+            email: {
+                required: "Email không được để trống",
+                email: "Email không đúng định dạng"
+            },
+            password: {
+                required: "Mật khẩu không được để trống",
+                minlength: "Mật khẩu phải có ít nhất 8 ký tự"
+            },
+            request:{
+                required: "Mật khẩu không được để trống",
+                minlength: "Mật khẩu phải có ít nhất 8 ký tự"
+            }
+        },
+        submitHandler: function (form) {
+            //code in here
+        }
+
+    });
+})
+
